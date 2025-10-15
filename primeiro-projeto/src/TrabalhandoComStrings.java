ff1af2a7e00815ccb85d847ea6a4ea7a5ebf1cfe
nunorulz <nunorulz@gmail.com>
2025-10-15 17:26:32 -0300

    Depois do merge

--- primeiro-projeto/src/TrabalhandoComStrings.java
+++ primeiro-projeto/src/TrabalhandoComStrings.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class TrabalhandoComStrings {
+    public static void main(String[] args) {
+
+        String nome = "Bruno";
+
+        int tamanhoDaString = nome.length();
+
+        System.out.println("Olá " + nome + " seu nome tem " + tamanhoDaString + " caracters");
+
+    }
 }
