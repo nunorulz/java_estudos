f31cfb0fde122b4234eceede430935274c0a7623
nunorulz <nunorulz@gmail.com>
2025-11-14 14:42:12 -0300

    Criação salvaUsuario

--- usuario/src/main/java/com/javanauta/usuario/business/UsuarioService.java
+++ usuario/src/main/java/com/javanauta/usuario/business/UsuarioService.java
@@ -1,5 +1,8 @@
 package com.javanauta.usuario.business;
 
+import com.javanauta.usuario.business.converter.UsuarioConverter;
+import com.javanauta.usuario.business.dto.UsuarioDTO;
+import com.javanauta.usuario.infrastructure.entity.Usuario;
 import com.javanauta.usuario.infrastructure.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,4 +12,11 @@ import org.springframework.stereotype.Service;
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+    private final UsuarioConverter usuarioConverter;
+
+    public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO){
+        Usuario usuario = usuarioConverter.paraUsuario(usuarioDTO);
+        return usuarioConverter.paraUsuarioDTO(usuarioRepository.save(usuario));
+    }
+
 }
--- usuario/src/main/java/com/javanauta/usuario/business/dto/TelefoneDTO.java
+++ usuario/src/main/java/com/javanauta/usuario/business/dto/TelefoneDTO.java
@@ -1,15 +1,13 @@
 package com.javanauta.usuario.business.dto;
 
 import jakarta.persistence.Column;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class TelefoneDTO {
 
     private Long id;
